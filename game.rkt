5af848e41a6c8a6371edb4baaeae710b10cea65e
Niks Gurins <niksdope@hotmail.com>
2017-03-13 15:50:07 +0000

    Added a function to generate permutations with repeating elements (for operators). Also trying out some stuff

--- game.rkt
+++ game.rkt
@@ -9,6 +9,9 @@ targetNumber
 ; The list that we will be populated with 6 random values
 (define randomNumbers null)
 
+; The list of all operators
+(define operators (list + - * /))
+
 ; A racket function that takes a list of values as a parameter and selects 6 random values from it, putting them in the randomNumbers list
 (define (getRandomNumbers l)
   (define n (list-ref l (random (length l))))
@@ -19,3 +22,32 @@ targetNumber
       (getRandomNumbers l)))
 
 (getRandomNumbers possibleNumbers)
+
+; http://stackoverflow.com/questions/13978595/getting-all-possible-combinations-of-x-booleans-racket-scheme
+(define (permutationsWithRepeats size elements)
+  (if (zero? size)
+      '(())
+      (append-map (lambda (p)
+                    (map (lambda (e)
+                           (cons e p))
+                         elements))
+                  (permutationsWithRepeats (sub1 size) elements))))
+
+(define numberPerms (combinations (permutations randomNumbers)))
+numberPerms
+
+;(define operatorPerms (permutationsWithRepeats 5 operators))
+
+(define combs null)
+
+;(define (combine nums ops)
+;  (if (null? ops)
+;      '(())
+;      (if (number? (car combs))
+;          (cons (car (car ops)) (combine nums (cdr (car ops))))
+;          (
+;  (if (null? nums)
+;      '(())
+;      (if (number? (car combs))
+;          (cons (car numbers) (combine (cdr numbers) operators))
+;          (cons 
